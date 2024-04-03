1a4765903a15fdf3ebd0fef8e91d41d3dd0e1c0e
abhijithcmr <abhijithcmr@gmail.com>
2024-04-03 15:20:12 +0530

    Finish basic frappe app

---
+++ library_management/library_management/doctype/library_membership/library_membership.py
@@ -0,0 +1,29 @@
+# Copyright (c) 2024, Abhijith P and contributors
+# For license information, please see license.txt
+
+import frappe
+from frappe.model.document import Document
+from frappe.model.docstatus import DocStatus
+
+
+class LIbraryMembership(Document):
+    '''
+    Docstring example
+    '''
+    def before_submit(self):
+        '''
+        Check before submitting the document
+        '''
+        exists = frappe.db.exists(
+			"Library Membership",{
+				"library_member": self.library_member,
+				"docstatus": DocStatus.submitted(),
+				# Check if the memberhip's end date is later than his membership's start date
+				"to_date": (">", self.from_date),
+			},
+		)
+        if exists:
+            frappe.throw("There is an active membership for this member")
+            
+        loan_period = frappe.db.get_single_value("Library Settings", "loan_period")
+        self.to_date = frappe.utils.add_days(self.from_date, loan_period or 30)
